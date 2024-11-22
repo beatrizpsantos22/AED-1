da079b8711465193d2a25a0cb5402d01415a8358
beatrizpsantos22 <bpsantos022@gmail.com>
2024-11-21 22:00:16 -0300

    Update Distância Entre Dois Pontos(1015).c

--- Distância Entre Dois Pontos(1015).c
+++ Distância Entre Dois Pontos(1015).c
@@ -1,17 +1,12 @@
 #include <stdio.h>
-
-int distancia (int x1, int x2, int y1, int y2){
-    return sqrt(pow(x2-x1,2)+pow(y2-y1,2));
-}
-
 int main() {
- int x1, x2, y1, y2, resposta;
+ double x1, x2, y1, y2, resposta;
  scanf("%lf", &x1);
  scanf("%lf", &x2);
  scanf("%lf", &y1);
  scanf("%lf", &y2);
  
- resposta = distancia;
+ resposta = sqrt(pow(x2-x1,2)+pow(y2-y1,2));
  
  printf("%.4lf\n", resposta);
     return 0;
